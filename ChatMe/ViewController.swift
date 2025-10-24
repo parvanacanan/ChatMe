16f645f4c1dd98a5583da8d3a37b8af80bccd34c
Parvana Jananova <parvanacanan@gmail.com>
2025-10-24 21:48:00 +0400

    Initial Commit

---
+++ ChatMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatMe
+//
+//  Created by parvana on 24.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
